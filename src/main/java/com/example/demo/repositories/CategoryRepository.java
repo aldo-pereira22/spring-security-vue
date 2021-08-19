93d17ecdebaecf069608e3bf57b114bb72d4953a
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-08-19 00:20:14 -0300

    Repositorio de Product e Category

--- src/main/java/com/example/demo/repositories/CategoryRepository.java
+++ src/main/java/com/example/demo/repositories/CategoryRepository.java
@@ -1,5 +1,10 @@
 package com.example.demo.repositories;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
 
 }
+ 
