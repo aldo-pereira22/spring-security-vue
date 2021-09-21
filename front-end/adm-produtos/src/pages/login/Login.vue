ae2d33378a5103af3c34804e415dd6d297245951
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-09-21 10:24:33 -0300

    Criando o formulário de login

--- front-end/adm-produtos/src/pages/login/Login.vue
+++ front-end/adm-produtos/src/pages/login/Login.vue
@@ -1,15 +1,45 @@
 <template>
   <div class="main">
+    <div class="login">
       <h2>Login</h2>
+      <label for="username">username</label>
+      <input type="text">
+      <label for="password">Password:</label>
+      <input type="password">
+      <button class="btn btn-primary">Login</button>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style scoped>
+    .main {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 2%;
+    }
+    .login {
+      display: flex;
+      flex-direction: column;
+      width: 40%;
+ background-color: #dceaf8;
+      padding: 4%;
+      border-radius: 12px;
 
+    }
+    input {
+      border: none;
+      border-bottom: solid 0.5px #070729;
+    }
+    input:focus{
+      outline: none;
+    }
+
+    button{
+      margin-top: 20px;
+    }
 </style>
