e3ad9d1d529603101b302389bac86ca14ce84c6a
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-09-22 20:37:30 -0300

    Finalizando apresentando produtos relacionados a cada categoria

--- front-end/adm-produtos/src/pages/categories/Categories.vue
+++ front-end/adm-produtos/src/pages/categories/Categories.vue
@@ -4,46 +4,42 @@
       <h1>Categories</h1>
     </div>
 
-    <div class="list" v-for="category in categories.categories " :key="category.id">
-      
-      <div class="dados"> {{ category.id }}</div>
-      <div class="dados"> {{ category.name }} </div>
-
-    </div>
-
-    <div class="row" v-for="produto in products.products" :key="produto.id">
-      <div class="produtos" v-if="produto.category">
-        {{produto}}
+    <div
+      class="list"
+      v-for="category in categories.categories"
+      :key="category.id"
+    >
+      <div class="categories">
+        <div class="dados">{{ category.id }}</div>
+        <div class="dados">
+          <button class="btn" v-on:click="mostraProdutos(category.id)">
+            {{ category.name }}
+          </button>
+        </div>
+      </div>
+      <div class="row" v-for="produto in products.products" :key="produto.id">
+        <div class="produtos" v-if="produto.category">
+          <ul v-if="produto.category.id == productId && category.id == productId">
+            <li>
+              {{ produto.name }}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-    <!-- <div class="vuex"> -->
-
-      <!-- <button v-on:click="addCount" >+</button>
-      <button v-on:click="minusCount" >-</button>
-    </div>
-      {{ contador }} -->
-
-      <hr>
-      <!-- <div class="produtos">
-        {{ products }}
-        <input v-model="produtos" type="text" name="" id="">
-        <button v-on:click="addProdutos(produtos)" class="btn btn-primary mt-4">+</button>
-      </div> -->
-
   </div>
 </template>
 
 <script>
-
 // import axios from 'axios'
-import {mapActions, mapState} from 'vuex'
+import { mapActions, mapState } from "vuex";
 export default {
-  data(){
+  data() {
     return {
-      produtos: ''
-    }
+      productId: "",
+    };
   },
-  methods:{
+  methods: {
     // getCategories(){
     //   axios.get('http://localhost:8080/categories').then( resp => {
     //     this.categories = resp.data;
@@ -57,45 +53,46 @@ export default {
     //   this.$store.commit('decrements')
     // },
     // ...mapActions(['addProdutos'])
-   ...mapActions(['getCategories']),
-   ...mapActions(['getProducts'])
-
-
+    ...mapActions(["getCategories"]),
+    ...mapActions(["getProducts"]),
+    mostraProdutos(id) {
+      id != this.productId ? (this.productId = id) : (this.productId = "");
+    
+    },
   },
-  created(){
+  created() {
     // this.getCategories();
-      this.getCategories(),
-      this.getProducts()
+    this.getCategories(), this.getProducts();
   },
-  computed:{
-
-    ...mapState([ 'categories', 'products'])
-  }
-
-}
+  computed: {
+    ...mapState(["categories", "products"]),
+  },
+};
 </script>
 
 <style scoped>
-
-
 .categories {
   display: flex;
   justify-content: center;
 }
 .list {
-    width: 50%;
-    margin-left: 22.5% ;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border:  1px solid gray;
-  }
+  width: 50%;
+  margin-left: 22.5%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid gray;
+}
 .dados {
   padding: 0.7%;
-
 }
 
-.vuex{
+ul {
+  list-style: none;
+}
+
+.vuex {
   display: flex;
   font-size: 30px;
   font-weight: 900;
@@ -103,7 +100,7 @@ export default {
   justify-content: center;
 }
 
-.produtos{
+.produtos {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -112,5 +109,12 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.categories {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: #dceaf8;
 }
 </style>
